7b0a30f8dc5f49244e235eb8e7fd8bafbf5d52a4
Mathieu Gravel <magravel@microsoft.com>
2025-07-02 14:36:31 -0700

    Adding sync version of CfuWriter (#21)
    This PR adds a sync version of `CfuWriter` and merged `CfuWriterMut`
    with `CfuWriter`.
    
    - The previous `CfuWriter` is now named `CfuWriterAsync` and the
    synchronous version is named `CfuWriterSync`. To keep the previous
    behavior, use `CfuWriterAsync`.
    - Rename the Default Writer to `CfuWriterNop` to be more explicit about
    what it does.
    - Removed default implementations that were not default behavior. The
    reason for the change, add clarity to the user about which function
    needs to be implemented to have a working writer and will "force" the
    use to implement these functions. In the event where someone wants a
    dummy implementation, the intent will be clearer now that it is moved
    where the implementation is.
    
    # Issues
    
    Fix #5  
    Fix #17

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ use core::future::Future;
 
 use embedded_io_async::{Read, ReadExactError, Seek, SeekFrom};
 
-use crate::components::*;
 use crate::protocol_definitions::*;
 
 pub mod client;
@@ -11,6 +10,7 @@ pub mod components;
 pub mod fmt;
 pub mod host;
 pub mod protocol_definitions;
+pub mod writer;
 
 // re-export the error enum
 pub use CfuProtocolError::*;
@@ -42,93 +42,4 @@ pub async fn read_from_exact<I: CfuImage>(
     image.read_exact(buf).await
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Ord, PartialOrd, Hash)]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub enum CfuWriterError {
-    StorageError,
-    ByteConversionError,
-    Other,
-}
-
-/// This is a temporary change and will be revisited later
-pub trait CfuWriterMut {
-    /// writes a chunk of data to a component and reads back to another buffer
-    fn cfu_write_read(
-        &mut self,
-        mem_offset: Option<usize>,
-        data: &[u8],
-        read: &mut [u8],
-    ) -> impl Future<Output = Result<(), CfuWriterError>>;
-    /// Fills a given buffer with data from the component
-    fn cfu_read(
-        &mut self,
-        mem_offset: Option<usize>,
-        read: &mut [u8],
-    ) -> impl Future<Output = Result<(), CfuWriterError>>;
-    /// Writes a given buffer of data to a component
-    /// Note: we don't need cfu_write as cfu_storage may replace it.
-    fn cfu_write(&mut self, mem_offset: Option<usize>, data: &[u8])
-        -> impl Future<Output = Result<(), CfuWriterError>>;
-    /// Manages erasing sectors and writing pages into flash based on the CFU offset
-    fn cfu_storage(&mut self, mem_offset: usize, data: &[u8]) -> impl Future<Output = Result<(), CfuWriterError>>;
-}
-
-/// Trait to define R/W behavior for driver that can talk to a CFU component or client
-pub trait CfuWriter {
-    /// writes a chunk of data to a component and reads back to another buffer
-    fn cfu_write_read(
-        &self,
-        mem_offset: Option<usize>,
-        data: &[u8],
-        read: &mut [u8],
-    ) -> impl Future<Output = Result<(), CfuWriterError>>;
-    /// Fills a given buffer with data from the component
-    fn cfu_read(&self, mem_offset: Option<usize>, read: &mut [u8]) -> impl Future<Output = Result<(), CfuWriterError>>;
-    /// Writes a given buffer of data to a component
-    fn cfu_write(&self, mem_offset: Option<usize>, data: &[u8]) -> impl Future<Output = Result<(), CfuWriterError>>;
-}
-
 pub type DataChunk = [u8; DEFAULT_DATA_LENGTH];
-
-#[derive(Copy, Clone)]
-pub struct CfuWriterDefault {}
-
-impl CfuWriterDefault {
-    /// Create new instance
-    pub fn new() -> Self {
-        Self {}
-    }
-}
-
-impl Default for CfuWriterDefault {
-    /// Creates default instance of CfuWriterDefault
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl CfuWriter for CfuWriterDefault {
-    /// writes a chunk of data to a component and reads back to another buffer
-    async fn cfu_write_read(&self, offset: Option<usize>, write: &[u8], read: &mut [u8]) -> Result<(), CfuWriterError> {
-        // TODO: add with_timeout to these calls
-        self.cfu_write(offset, write).await?;
-        self.cfu_read(offset, read).await?;
-        Ok(())
-    }
-    /// Fills a given buffer with 0xBE 0xEF alternating bytes
-    async fn cfu_read(&self, _offset: Option<usize>, read: &mut [u8]) -> Result<(), CfuWriterError> {
-        info!("Fake reading from component");
-        for (i, byte) in read.iter_mut().enumerate() {
-            if i % 2 == 0 {
-                *byte = 0xEF;
-            } else {
-                *byte = 0xBE;
-            }
-        }
-        Ok(())
-    }
-    async fn cfu_write(&self, _offset: Option<usize>, write: &[u8]) -> Result<(), CfuWriterError> {
-        info!("Fake writing to component: {:?}", write);
-        Ok(())
-    }
-}
